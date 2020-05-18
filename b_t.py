6c3ce48ef872057bb74d7057075c01035a678cc5
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-18 21:17:45 +0300

    Add files via upload

---
+++ b_t.py
@@ -0,0 +1,34 @@
+import aiogram as ai
+import logging
+import random
+TOKEN='1133381423:AAEytfr8xb5xoB9iewgDWPAwKZlMgkArW_w'
+
+
+logging.basicConfig(level=logging.INFO )
+bot=ai.Bot(token=TOKEN)
+dp=ai.Dispatcher(bot )
+menu=ai.types.ReplyKeyboardMarkup(
+    keyboard=[
+    [
+        ai.types.KeyboardButton(text="Да")
+    ],
+        [
+            ai.types.KeyboardButton(text="Нет")
+        ]
+    ], resize_keyboard=True )
+
+@dp.message_handler(commands=['start'])
+async def send_mes(message: ai.types.message):
+    text = "Привет, тебе " + str(random.randint(1, 100)) + "?"
+    await message.answer(text, reply_markup=menu)
+
+@dp.message_handler(ai.dispatcher.filters.Text(equals=["Да","Нет"]))
+async def ans(message: ai.types.message):
+    if (message.text == "Да"):
+        await message.answer("Хорошо!", reply_markup=ai.types.ReplyKeyboardRemove())
+    else:
+        await message.answer("Ну и пошёл вон!", reply_markup=ai.types.ReplyKeyboardRemove())
+
+
+if __name__ =='__main__':
+    ai.executor.start_polling(dp, skip_updates= True )
