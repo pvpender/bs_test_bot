3b6354ea3d38a6ffcab62e78a325bdfa2027bbe1
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-18 20:39:18 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord as ds
-TOKEN = 'NzExNTMxMjI3MDkzNDAxNjkw.XsGIaw.O6_OqDuz_yDGTbHPTh4M0xZgXYg'
-client=ds.Client()
+import os
+TOKEN = os.environ.get('B_T')
+client = ds.Client()
 
 @client.event
 async def on_ready():
@@ -10,9 +11,6 @@ async def on_ready():
 async def on_message(message):
 
     if message.content.startswith('hello') or message.content.startswith('hi') or message.content.startswith('привет') or message.content.startswith('дарова') or message.content.startswith('Hello') or message.content.startswith('Hi') or message.content.startswith('Привет') or message.content.startswith('Дарова'):
-       await message.channel.send('Привет, раб системы')
-
-
-
+     await message.channel.send('Привет, раб системы')
 
 client.run(TOKEN)
