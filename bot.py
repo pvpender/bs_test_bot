8d98ea094a8c0309b3ba5a73feaccaffad5cac87
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-07-28 19:15:44 +0300

    New

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord as ds
+from discord.abc import User, GuildChannel, Messageable
+from discord import user
 import os
+from datetime import datetime
+
 TOKEN = os.environ.get('B_T')
 client = ds.Client()
 
@@ -14,7 +18,7 @@ async def on_message(message):
         await message.channel.send('Привет, раб системы')
 
 @client.event
-async def on_typing(message):
-    await message.channel.send(f"Чё пишешь, {client.user.id} ")
+async def on_typing(Messageable, user, datetime):
+    await Messageable.send(f"Net ne pishi {user.name}")
 
 client.run(TOKEN)
